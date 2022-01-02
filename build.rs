af9d4cdaa0fe13eda6a674bdc383b69468070aa7
Elad Kaplan <eladkaplan@Elads-MacBook-Pro-2.local>
2022-01-02 10:46:55 +0200

    improve build.rs

--- build.rs
+++ build.rs
@@ -1,14 +1,15 @@
 use anyhow::Result;
+use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
 fn main() -> Result<()> {
-    let destination_file = "./src/checks.yaml";
-    if Path::new(&destination_file).exists() {
-        fs::remove_file(&destination_file)?;
-    }
+    println!("cargo:rerun-if-changed=checks/");
+
+    let out_dir = env::var("OUT_DIR")?;
+    let dest_checks_path = Path::new(&out_dir).join("all-checks.yaml");
 
     let paths = fs::read_dir("./checks")?;
     let mut all_group_checks = String::new();
@@ -18,7 +19,7 @@ fn main() -> Result<()> {
         all_group_checks.push('\n');
     }
 
-    let mut file = File::create(destination_file)?;
+    let mut file = File::create(dest_checks_path)?;
     file.write_all(all_group_checks.as_bytes())?;
 
     Ok(())
